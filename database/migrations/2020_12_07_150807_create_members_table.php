eb108b1b7988c8e63e46ceab684e11f62e11e968
Rian A <rianiregho@gmail.com>
2020-12-07 22:11:40 +0700

    Add new migration for Member

---
+++ database/migrations/2020_12_07_150807_create_members_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            if(config('database.default') == 'pgsql')
+                $table->string('uid')->default(DB::raw('gen_uuid()'))->primary();
+            else if(config('database.default') == 'mysql')
+                $table->string('uid')->default(DB::raw('uuid()'))->primary();
+            $table->string('national_id');
+            $table->integer('province_id');
+            $table->integer('city_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('idcard_image');
+            $table->string('selfie_idcard_image');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
