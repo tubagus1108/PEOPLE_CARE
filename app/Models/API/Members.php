0dcaa25b7ede918704dd0ee1d64298198a4e7e6e
Rian Iregho <rianiregho@gmail.com>
2021-01-12 20:24:55 +0700

    '

--- app/Models/API/Members.php
+++ app/Models/API/Members.php
@@ -5,6 +5,7 @@ namespace App\Models\API;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Passport\HasApiTokens;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Members extends Authenticatable
 {
