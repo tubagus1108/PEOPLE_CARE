0558c506501dddcd234f6411717fd3e49f50a054
Rian Iregho <rianiregho@gmail.com>
2021-01-12 20:18:51 +0700

    '

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AdminController;
 use App\Http\Controllers\API\TerritoryController;
 use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\MemberController;
 
 /*
 |--------------------------------------------------------------------------
